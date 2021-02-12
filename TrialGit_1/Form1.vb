a95bc8e004a1bd2b129a9d7c94a10f7ca60b3661
HeavHarv <heavenlokaputra@gmail.com>
2021-02-12 17:59:49 +0700

    KROKOT

--- TrialGit_1/Form1.vb
+++ TrialGit_1/Form1.vb
@@ -2,4 +2,12 @@
     Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs) Handles TextBox1.TextChanged
 
     End Sub
+
+    Private Sub Label2_Click(sender As Object, e As EventArgs) Handles Label2.Click
+
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Label2.Text = "KROKOT"
+    End Sub
 End Class
