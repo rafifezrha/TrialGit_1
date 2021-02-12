0fa159a86e97f4a1b69d5b701cd5cb4b4117019c
PC-MESIN-12 <PC-MESIN-12@DESKTOP-H46G3LH>
2021-02-12 17:49:52 +0700

    add button

--- TrialGit_1/Form1.vb
+++ TrialGit_1/Form1.vb
@@ -2,4 +2,8 @@
     Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs) Handles TextBox1.TextChanged
 
     End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
 End Class
